6a402fdf83f3c29267336833c30ab56962719807
limichange <limichange@hotmail.com>
2016-12-16 16:42:37 +0800

    update

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('disconnect')
+    io.emit('user disconnect');
+  });
 
   socket.on('send message', function(msg){
-    console.log('message: ' + msg);
-
+    console.log('get message')
     io.emit('get message', msg);
   });
 });
